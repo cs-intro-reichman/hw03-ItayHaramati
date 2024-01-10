b1cca4330ecc531f881ab55b97b8ccda54992de7
Itay <you@example.com>
2024-01-10 18:49:08 +0200

    I changed UniqueChars

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,14 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String temp = " ";
+        for (int i = 0; i < s.length(); i++){
+           if (temp.indexOf(s.charAt(i)) == - 1){
+              temp = temp + s.charAt(i);
+        }
+     }
+
+   return temp;
+
+  }        
     }
-}
