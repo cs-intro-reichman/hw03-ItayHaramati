bff8f97e8d3e54d27dc5538fda50abaf8fa0faef
Itay <you@example.com>
2024-01-10 13:47:59 +0200

    I changed LowerCase

--- LowerCase.java
+++ LowerCase.java
@@ -1,8 +1,8 @@
 /** String processing exercise 1. */
 public class LowerCase {
     public static void main(String[] args) {  
-        String str = args[0];
-        System.out.println(lowerCase(str));
+        String result = args[0];
+        System.out.println(lowerCase(result));
     }
 
    /**
@@ -11,7 +11,21 @@ public class LowerCase {
     * Non-letter characters are left as is.
     */
     public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+            String result = " ";
+            char ch = ' ';
+            for (int i = 0; i < s.length(); i++) {
+                
+                //check valid alphabet and it is in Uppercase
+                if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                    ch = (char)(s.charAt(i) + 32);
+                }
+                //else keep the same alphabet or any character
+                else {
+                    ch = (char)(s.charAt(i));
+                }
+                
+                result += ch; // concatenation, append c to result
+            }
+            return result;
+        }
     }
-}
